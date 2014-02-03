b49e1e9bd3614d16720a7b64ba3030fa20350674
dovadi <frank.oxener@gmail.com>
2014-02-03 12:59:00 +0100

    Fix participation test

--- test/models/participant_test.rb
+++ test/models/participant_test.rb
@@ -4,7 +4,7 @@ describe Participant do
 
   it 'should validate presence of lastname' do
     participant = Participant.create(:firstname => 'Frank')
-    participant.errors[:lastname].must_equal ["can't be blank"]
+    participant.errors[:lastname].must_equal ["moet opgegeven zijn"]
   end
 
 end
