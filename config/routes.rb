9fe8f152f092960d09615be1beb36cb72ca87275
dovadi <frank.oxener@gmail.com>
2014-03-24 13:37:16 +0100

    Root route changed to participants

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Alijv::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'participations#index'
+  root 'participants#index'
 
 
   # Example of regular route:
