8b7352f1b527962c645d29f0fd83d6a554457953
Bas Stam <sebastiaan.stam@gmail.com>
2014-04-08 07:07:36 +0200

    BR no more then 1 activity active

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,6 +4,14 @@ class Activity < ActiveRecord::Base
 
   validates :name, :description, presence: true
 
+  after_save :validate_maximum_active_activities
+
   scope :active, -> { where(active: true).first }
 
+  def validate_maximum_active_activities
+    if Activity.where(active: true).count > 1
+       self.errors[:base] = 'Er mag maximaal 1 activiteit actief zijn.'
+       raise ActiveRecord::Rollback
+    end
+  end
 end
--- test/models/activity_test.rb
+++ test/models/activity_test.rb
@@ -10,8 +10,17 @@ describe Activity do
 
   it { @activity.must validate_presence_of(:name) }
 
-  it 'should return the active activity' do
-    Activity.active.name.must_equal 'Veldloop 2015'
+  
+  describe 'in case of more then one active activity' do
+    before do
+      Activity.destroy_all
+      Activity.create(name: "veldloop a", description: "velloop a", active: true)
+      Activity.create(name: "veldloop b", description: "velloop b", active: true)
+      #Activity.where(id: 1).update_all(active: true)
+    end
+      
+    it 'should add an error' do
+      @activity.errors[:base].must_equal ['Er mag maximaal 1 activiteit actief zijn.']
+    end
   end
-
 end
