964833f1c869c1f25e65e00385a895d1b8a93591
dovadi <frank.oxener@gmail.com>
2014-04-14 14:48:50 +0200

    Fix activity test

--- test/models/activity_test.rb
+++ test/models/activity_test.rb
@@ -14,14 +14,11 @@ describe Activity do
   
   describe 'in case of more then one active activity' do
     before do
-      Activity.destroy_all
-      Activity.create(name: "veldloop a", description: "velloop a", start_date: "2014-03-27 10:00:00", active: true)
-      Activity.create(name: "veldloop b", description: "velloop b", start_date: "2014-02-27 10:00:00", active: true)
-      #Activity.where(id: 1).update_all(active: true)
+      @second_active_activity = Activity.create(name: "veldloop a", description: "velloop a", start_date: "2014-03-27 10:00:00", active: true)
     end
       
     it 'should add an error' do
-      @activity.errors[:base].must_equal ['Er mag maximaal 1 activiteit actief zijn.']
+      @second_active_activity.errors[:base].must_equal ['Er mag maximaal 1 activiteit actief zijn.']
     end
   end
 end
