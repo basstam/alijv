e0d59353ad1a5a3b70651a8205a31e8409114d26
Bas Stam <sebastiaan.stam@gmail.com>
2014-04-18 13:03:57 +0200

    BR added activity startdate must be before enddate

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,14 +4,23 @@ class Activity < ActiveRecord::Base
 
   validates :name, :description, :start_date, presence: true
 
+  validate :start_date_before_end_date?
+
   after_save :validate_maximum_active_activities
 
   scope :active, -> { where(active: true).first }
 
+
+  def start_date_before_end_date?
+    if end_date.present? && start_date > end_date
+      self.errors.add(:start_date, 'moet kleiner of gelijk zijn dan de einddatum')
+    end
+  end
+
   def validate_maximum_active_activities
     if Activity.where(active: true).count > 1
-       self.errors[:base] = 'Er mag maximaal 1 activiteit actief zijn.'
-       raise ActiveRecord::Rollback
+      self.errors[:base] = 'Er mag maximaal 1 activiteit actief zijn.'
+      raise ActiveRecord::Rollback
     end
   end
 end
--- test/models/activity_test.rb
+++ test/models/activity_test.rb
@@ -11,7 +11,21 @@ describe Activity do
   it { @activity.must validate_presence_of(:name) }
 
   it { @activity.must validate_presence_of(:start_date) }
-  
+
+  describe 'Validate start_date before end_date' do
+    before do
+      @activity = Activity.create(name: "veldloop a", description: "velloop a", start_date: "2014-03-27", end_date: "2014-03-26")
+    end
+      
+    it 'should add an error' do
+      @activity.errors[:start_date].must_equal ['moet kleiner of gelijk zijn dan de einddatum']
+    end
+
+    it 'should not save the record' do
+      Activity.count.must_equal 3 #Number of records in fixtures
+    end
+  end
+
   describe 'in case of more then one active activity' do
     before do
       @second_active_activity = Activity.create(name: "veldloop a", description: "velloop a", start_date: "2014-03-27 10:00:00", active: true)
