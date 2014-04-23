99fb49f6aafc3e9680d8a02b9d12edb092f737b4
Bas Stam <sebastiaan.stam@gmail.com>
2014-04-23 17:53:57 +0200

    BR added update paticipation not allowed when activity is closed

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,9 +1,18 @@
 class Participation < ActiveRecord::Base
-  belongs_to :activity
-  belongs_to :category
-  belongs_to :participant
+  belongs_to  :activity
+  belongs_to  :category
+  belongs_to  :participant
 
-  validates :activity_id, :participant_id, :category_id,  presence: true
+  validates   :activity_id, :participant_id, :category_id,  presence: true
+
+  before_update :update_allowed? 
 
   delegate :distance, to: :category
+
+  def update_allowed?
+    activity = Activity.find(self.activity_id)
+    if activity.end_date.present?  && activity.end_date < Time.now.utc.to_date
+      self.errors.add(:base, 'Deelname kan niet meer worden bijgewerkt, activiteit is afgesloten')
+    end
+  end
 end
--- test/models/participation_test.rb
+++ test/models/participation_test.rb
@@ -14,4 +14,23 @@ describe Participation do
   it { @participation.must validate_presence_of(:participant_id) }
   it { @participation.must validate_presence_of(:category_id) }
 
+  describe 'In case of a update' do
+    before do
+      # in fixtures
+      # start_date: "2014-03-27 10:00:00"
+      # end_date: "2014-03-28 10:00:00"
+      participant_one = participants(:one)
+      @participation  = Participation.create(participant_id: participants(:one).id,
+                                             category_id: categories(:two).id,
+                                             activity_id: Activity.active.id )
+    end
+
+    it 'should raise a error when associate activity is closed' do
+      @participation.update_attribute(:end_time, "2014-01-27 10:00:00")
+      @participation.errors[:base].must_equal ['Deelname kan niet meer worden bijgewerkt, activiteit is afgesloten']
+    end
+    
+  end
+
+
 end
