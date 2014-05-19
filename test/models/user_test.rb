320349ef28bd2ed5416aa8b1cac968555a44f86e
Bas Stam <sebastiaan.stam@gmail.com>
2014-05-19 15:22:30 +0200

    WIP: use devise for authentication, tests don't work yet with devise and warden test helpers

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,10 +2,11 @@ require "test_helper"
 
 describe User do
   before do
-    @user = User.new
+    @user = User.create!(:email => 'bas@dovadi.nl', :password => 'admin1234')
   end
 
   it "must be valid" do
     @user.valid?.must_equal true
   end
 end
+
