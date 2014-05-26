7b1ce2d820778ef3b02930df02bf2cedebcdbe8f
Bas Stam <sebastiaan.stam@gmail.com>
2014-05-26 14:10:27 +0200

    Email settings for developmentadded

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,4 +29,13 @@ Alijv::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  #
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => 'sebastiaan.stam@gmail.com',
+  :password             => ENV['ALIJV_GMAIL_PASSWORD'],
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
 end
